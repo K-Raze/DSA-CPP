f0584af54493a72aeea51107b263e7e5f7ef8022
Kartik <dhamijakartik2005@gmail.com>
2026-07-02 21:25:32 +0530

    Time: 8 ms (89.21%), Space: 32.3 MB (51.31%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,43 +1,49 @@
 class Solution {
 public:
 
+    int r[4]={0,0,1,-1};
+    int c[4]={1,-1,0,0};
+
     int n,m;
 
-    int r[4]={1,-1,0,0};
-    int c[4]={0,0,-1,1};
-
-    bool isValid(int x,int y)
+    inline bool isValid(int x,int y)
     {
         return x>=0 && y>=0 && x<n && y<m;
     }
 
+    bool findSafeWalk(vector<vector<int>>& mat, int health) {
+        n=mat.size();
+        m=mat[0].size();
 
-    bool findSafeWalk(vector<vector<int>>& mat, int h) {
-        n=mat.size(),m=mat[0].size();
+        vector<vector<int>>healthCell(n,vector<int>(m,-1));
 
-        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
-        vector<vector<int>> cost(n,vector<int>(m,INT_MAX));
-        vector<vector<bool>> vis(n,vector<bool>(m,0));
-        pq.push({mat[0][0],0,0});
-        cost[0][0]=mat[0][0];
-        while(!pq.empty())
+        deque<pair<int,int>>dq;
+        dq.push_front({0,0});
+        healthCell[0][0]=health-mat[0][0];
+
+        while(!dq.empty())
         {
-            auto[cs,x,y]=pq.top();
-            pq.pop();
-            if(cs!=cost[x][y] || vis[x][y])
-                continue;
-            vis[x][y]=1;
+            auto[x,y]=dq.front();
+            dq.pop_front();
+            if(x==n-1 && y==m-1)
+                return healthCell[x][y]>=1;
+            if(!healthCell[x][y])
+                return 0;
+            
             for(int k=0;k<4;k++)
             {
                 int nx=x+r[k];
                 int ny=y+c[k];
-                if(isValid(nx,ny) && !vis[nx][ny] && cs+mat[nx][ny]<cost[nx][ny])
+                if(isValid(nx,ny) && healthCell[nx][ny]==-1)
                 {
-                    cost[nx][ny]=cs+mat[nx][ny];
-                    pq.push({cost[nx][ny],nx,ny});
+                    healthCell[nx][ny]=healthCell[x][y]-mat[nx][ny];
+                    if(mat[nx][ny])
+                        dq.push_back({nx,ny});
+                    else
+                        dq.push_front({nx,ny});
                 }
             }
         }
-        return (h-cost[n-1][m-1])>=1;
+        return 0;
     }
 };
