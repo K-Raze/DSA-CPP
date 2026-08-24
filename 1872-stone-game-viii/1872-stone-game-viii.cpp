048bb4aaf3c80409f003ce63516ed7dca2dbdfba
Kartik <dhamijakartik2005@gmail.com>
2026-08-24 15:07:50 +0530

    Time: 132 ms (8.79%), Space: 95.9 MB (40.56%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -8,68 +8,38 @@ public:
     // (a+b+c) d e //.    +(a+b+c+d+e)
     // (a+b+c+d+e) //.    
 
-    // int solve(int x,int turn,vector<int>& pre)
-    // {
-    //     if(x==pre.size())
-    //         return 0;
-    //     int ans;
-    //     if(turn) // alice max krega
-    //     {
-    //         ans=-inf;
-    //         for(int i=max(1,x);i<pre.size();i++)
-    //             ans=max(ans,pre[i]+solve(i+1,1-turn,pre));
-    //     }
-    //     else
-    //     {
-    //         ans=inf;
-    //         for(int i=max(1,x);i<pre.size();i++)
-    //             ans=min(ans,-pre[i]+solve(i+1,1-turn,pre));
-    //     }
-    //     return ans;
-    // }
+    int dp[100005][2];
+
+    int solve(int x,bool turn,vector<int>& pre)
+    {
+        if(x==pre.size()-1)
+        {
+            if(turn)
+                return pre[x];
+            return -pre[x];
+        }
+
+        if(dp[x][turn]!=-1)
+            return dp[x][turn];
+
+        int ans;
+        if(turn)
+        {
+            ans=pre[x]+solve(x+1,1-turn,pre);
+            ans=max(ans,solve(x+1,turn,pre));
+        }
+        else
+        {
+            ans=-pre[x]+solve(x+1,1-turn,pre);
+            ans=min(ans,solve(x+1,turn,pre));
+        }
+        return dp[x][turn]=ans;
+    }
 
     int stoneGameVIII(vector<int>& arr) {
-        int n=arr.size();
-        vector<int>pre=arr;
-        for(int i=1;i<n;i++)
-            pre[i]+=pre[i-1];
-
-        int dp[n+1][2];
-        dp[n][0]=dp[n][1]=0;
-        int predp[2][n+1];
-        predp[0][n]=inf;
-        predp[1][n]=-inf;
-        for(int x=n-1;x>=0;x--)
-            for(int turn=0;turn<=1;turn++)
-            {   
-                if(turn)
-                {
-                    predp[turn][x]=pre[x]+dp[x+1][1-turn];
-                    predp[turn][x]=max(predp[turn][x],predp[turn][x+1]);
-                }
-                else
-                {
-                    predp[turn][x]=-pre[x]+dp[x+1][1-turn];
-                    predp[turn][x]=min(predp[turn][x],predp[turn][x+1]);
-                }
-                int ans;
-                if(turn) // alice max krega
-                {
-                    ans=-inf;
-                    // for(int i=max(1,x);i<pre.size();i++)
-                    //     ans=max(ans,pre[i]+dp[i+1][1-turn]);
-                    ans=max(ans,predp[turn][max(1,x)]);
-                }
-                else
-                {
-                    ans=inf;
-                    // for(int i=max(1,x);i<pre.size();i++)
-                    //     ans=min(ans,-pre[i]+dp[i+1][1-turn]);
-                    ans=min(ans,predp[turn][max(1,x)]);
-                }
-                dp[x][turn]=ans;
-            }
-
-        return dp[0][1];
+        for(int i=1;i<arr.size();i++)
+            arr[i]+=arr[i-1];
+        memset(dp,-1,sizeof(dp));
+        return solve(1,1,arr);
     }
 };
