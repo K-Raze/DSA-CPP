998521827a4a9f9cf9569393ca60122ad581b639
Kartik <dhamijakartik2005@gmail.com>
2026-08-26 00:45:54 +0530

    Time: 805 ms (16.04%), Space: 60.4 MB (11.69%) - LeetHub

---
+++ 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minTrioDegree(int n, vector<vector<int>>& edges) {
+        vector<unordered_set<int>>adj(n);
+        vector<int>deg(n,0);
+        for(auto& it : edges)
+        {
+            it[0]--;
+            it[1]--;
+            adj[it[0]].insert(it[1]);
+            adj[it[1]].insert(it[0]);
+            deg[it[0]]++;
+            deg[it[1]]++;
+        }
+
+        int ans=2e9;
+
+        for(int i=0;i<n;i++)
+            for(int j=i+1;j<n;j++)
+                for(int k=j+1;k<n;k++)
+                    if(adj[i].find(j)!=adj[i].end() && adj[i].find(k)!=adj[i].end()
+                        && adj[j].find(k)!=adj[j].end())
+                            ans=min(ans,deg[i]+deg[j]+deg[k]-6);
+        if(ans==2e9)
+            return -1;
+        return ans;
+    }
+};
