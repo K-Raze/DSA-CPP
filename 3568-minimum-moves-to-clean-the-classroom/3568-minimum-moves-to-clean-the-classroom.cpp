4203c13f97705ddbf49f3b2e1f818655f75b22da
Kartik <dhamijakartik2005@gmail.com>
2026-09-02 00:12:38 +0530

    Time: 2741 ms (5.1%), Space: 421.8 MB (28.57%) - LeetHub

---
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.cpp
@@ -0,0 +1,104 @@
+bool vis[21][21][51][1024];
+const int inf=2e9;
+class Solution {
+public:
+
+    int n,m;
+    int r[4]={0,0,1,-1};
+    int c[4]={1,-1,0,0};
+
+    inline bool isValid(int x,int y)
+    {
+        return x>=0 && y>=0 && x<n && y<m;
+    }
+
+
+    const int inf=2e9;
+
+    int minMoves(vector<string>& mat, int E) {
+        n=mat.size();
+        m=mat[0].size();
+
+        vector<vector<int>> cordBit(n, vector<int>(m, -1));
+        int tl;
+        {
+            int bit=0;
+            for(int i=0;i<n;i++)
+                for(int j=0;j<m;j++)
+                    if(mat[i][j]=='L')
+                        cordBit[i][j]=bit++;
+            tl=bit;
+        }
+        if(!tl)
+            return 0;
+
+        int fullMask = (1 << tl) - 1;
+        {
+            for(int i=0;i<n;i++)
+                for(int j=0;j<m;j++)
+                    for(int e=0;e<=E;e++)
+                        for(int k=0;k<=fullMask;k++)
+                            vis[i][j][e][k]=0;
+        }
+        
+        queue<array<int,5>>q;
+        {
+            for(int i=0;i<n;i++)
+                for(int j=0;j<m;j++)
+                    if(mat[i][j]=='S')
+                    {
+                        q.push({0,E,i,j,fullMask});
+                        vis[i][j][E][fullMask]=1;
+                        break;
+                    }
+        }
+        // move,energy,x,y,mask
+        int ans=inf;
+        while(!q.empty())
+        {
+            auto [move,ce,x,y,mask]=q.front();
+            q.pop();
+            if(!mask)
+                ans=min(ans,move);
+            if(!ce || !mask)
+                continue;
+            for(int k=0;k<4;k++)
+            {
+                int nx=x+r[k];
+                int ny=y+c[k];
+                if(isValid(nx,ny) && mat[nx][ny]!='X')
+                {
+                    if(mat[nx][ny]=='L')
+                    {
+                        int bit=cordBit[nx][ny];
+                        int nMask = mask & ~(1 << bit);
+                        if(!vis[nx][ny][ce-1][nMask])
+                        {
+                            vis[nx][ny][ce-1][nMask]=1;
+                            q.push({move+1,ce-1,nx,ny,nMask});
+                        }
+                    }
+                    else if(mat[nx][ny]=='R')
+                    {
+                        if(!vis[nx][ny][E][mask])
+                        {
+                            vis[nx][ny][E][mask]=1+move;
+                            q.push({move+1,E,nx,ny,mask});
+                        }
+                    }
+                    else
+                    {
+                        if(!vis[nx][ny][ce-1][mask])
+                        {
+                            vis[nx][ny][ce-1][mask]=1;
+                            q.push({move+1,ce-1,nx,ny,mask});
+                        }
+                    }
+                }
+            }
+        }
+        if(ans==inf)
+            return -1;
+        return ans;
+    }
+};
