b2123b639e8defda03d88cec38a2d3dcda5737c9
Kartik <dhamijakartik2005@gmail.com>
2026-09-20 10:57:29 +0530

    Time: 0 ms (100%), Space: 9.7 MB (47.88%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans=0;
+        for(int i=1;i<=s.size();i++)
+            ans+=i*('z'-s[i-1]+1);
+        return ans;
+    }
+};
